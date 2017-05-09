39982ea9b18530d861fc8af2025e50e9342e3211
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-05-09 18:58:16 +0200

    Added load_model option and class weights

--- cnn.py
+++ cnn.py
@@ -1,4 +1,5 @@
 from keras.models import Sequential
+from keras.models import load_model
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import AveragePooling2D
@@ -11,53 +12,60 @@ from keras.optimizers import SGD
 
 class KanjiRecognizer(object):
 
-	def __init__(self, output_size, image_size, learning_rate, epochs):
-		self.image_size = image_size
-		self.learning_rate = learning_rate
-		self.epochs = epochs
-		self.output_size = output_size
+    def __init__(self, output_size, image_size, learning_rate, epochs):
+        self.image_size = image_size
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.output_size = output_size
 
-	def build_model(self):
-		model = Sequential()
-		model.add(Conv2D(32, (3, 3), input_shape=(self.image_size, self.image_size, 3)))
-		model.add(Activation('relu'))
-		model.add(Conv2D(32, (3, 3)))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
+    def build_model(self):
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), input_shape=(self.image_size, self.image_size, 3)))
+        model.add(Activation('relu'))
+        model.add(Conv2D(32, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
 
-		model.add(Conv2D(64, (3, 3)))
-		model.add(Activation('relu'))
-		model.add(Conv2D(64, (3, 3)))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(64, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
 
-		model.add(Conv2D(128, (3, 3)))
-		model.add(Activation('relu'))
-		model.add(Conv2D(128, (3, 3)))
-		model.add(Activation('relu'))
+        model.add(Conv2D(128, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(Conv2D(128, (3, 3)))
+        model.add(Activation('relu'))
 
 
-		model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-		model.add(Activation('relu'))
-		model.add(Dropout(0.5))
-		model.add(Dense(self.output_size))
-		model.add(Activation('softmax'))
+        model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.output_size))
+        model.add(Activation('softmax'))
 
-		optimizer = SGD(lr=self.learning_rate, momentum=0.9, decay=0.0, nesterov=True)
-		model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-		self.model = model
-		model.summary()
-		input()
+        optimizer = SGD(lr=self.learning_rate, momentum=0.9, decay=0.0, nesterov=True)
+        model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        self.model = model
+        model.summary()
+        input()
+        
+    def load_model(self, weights_path):
+        self.model = load_model(weights_path)
 
-	def fit(self, train_data, val_data, nb_train_samples, nb_val_samples, callbacks):
-		return self.model.fit_generator(
+    def fit(self, train_data, val_data, nb_train_samples, nb_val_samples, callbacks, class_weights=None):
+        return self.model.fit_generator(
             train_data,
             samples_per_epoch=nb_train_samples,
             nb_epoch=self.epochs,
             validation_data=val_data,
             nb_val_samples=nb_val_samples,
-            callbacks=callbacks)
+            callbacks=callbacks,
+            class_weight=class_weights)
 
-	def predict(self, test_data, nb_test_samples):
-	    return self.model.predict_generator(test_data, nb_test_samples)
-		
+    def predict(self, sample):
+        return self.model.predict(sample)
+
+    def predict_generator(self, test_data, nb_test_samples):
+        return self.model.predict_generator(test_data, nb_test_samples)
+        
