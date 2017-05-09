b160710101fa3d0dde1355163963be20c9b85669
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-05-09 15:19:41 +0200

    Optimized dataset generator (*50 speed)

--- dataset_generator.py
+++ dataset_generator.py
@@ -6,8 +6,9 @@ import os
 import config
 
 if __name__ == "__main__":
-    DEST_DIR = "data3\\val"#config.VAL_DIR
-    FONT = config.FONT_ARIAL
+    DEST_DIR = "data3\\train"#config.VAL_DIR
+    FONTS = config.FONTS
+    IMAGE_FONTS = [ImageFont.truetype(font_file, config.FONT_SIZE) for font_file in FONTS]
 
     with open(config.KANJI_LIST, "r", encoding="utf8") as f:
         kanjis = f.read().split("\n")
@@ -15,24 +16,34 @@ if __name__ == "__main__":
     while "" in kanjis:
         kanjis.remove("")
 
-    j = 0
     for kanji in kanjis:
-        #print(kanji)
-        i=0
-        for font_file in config.VAL_FONTS:
-            image = Image.new("RGB", config.IMAGE_SIZE, config.BACKGROUND)
-            draw = ImageDraw.Draw(image)
-            font = ImageFont.truetype(font_file, config.FONT_SIZE)
-            draw.text (config.TEXT_OFFSET, kanji, font=font, fill=config.COLOR)
-            directory = os.path.join(DEST_DIR, kanji)
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            file_path = os.path.join(directory, font_file + ".jpg")#kanji + str(i) + ".jpg")
+        directory = os.path.join(DEST_DIR, kanji)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
+    import time
+
+    start = time.time()
+
+
+
+    j = 0
+    orig = Image.new("RGB", config.IMAGE_SIZE, config.BACKGROUND)
+    for kanji in kanjis:
+        directory = os.path.join(DEST_DIR, kanji)
+        i=0
+        for font in IMAGE_FONTS:
+            image = orig.copy()
+            draw = ImageDraw.Draw(image)
+            draw.text (config.TEXT_OFFSET, kanji, font=font, fill=config.COLOR)
+            file_path = os.path.join(directory, str(i) + ".jpg")#kanji + str(i) + ".jpg")
             image.save(file_path, "JPEG")
             i += 1
         j += 1
-        if j % 100 == 0:
+        if j % 500 == 0:
+            end = time.time()
+            print(end - start)
+            start = time.time()
             print(j)
 
 
