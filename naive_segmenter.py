6b257a6354cddb7518ea2fff792da712cc410649
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-05-09 03:56:19 +0200

    Added max character size to segmenter

--- naive_segmenter.py
+++ naive_segmenter.py
@@ -5,10 +5,12 @@ import math
 
 class NaiveSegmenter(Segmenter):
 
-	def __init__(self, text_bounding_box, line_width, character_size):
+	def __init__(self, text_bounding_box, line_width, char_min_size, char_max_size):
 		self.text_bounding_box = text_bounding_box
 		self.line_width = line_width
-		self.character_size = character_size
+		self.char_min_size = char_min_size
+		self.char_max_size = char_max_size
+		self.too_big_rectifier = 10
 
 	def get_screen_capture(self):
 		# TODO: LINUX
@@ -39,15 +41,24 @@ class NaiveSegmenter(Segmenter):
 
 			line_height = 0
 			last_line_drawn = 0
+			last_was_too_big = False
 			while line_height < im.size[1]:
 				intersects_character = True
 				size_too_small = True
-				while intersects_character or size_too_small:
+				size_too_big = False
+				while not size_too_big and (intersects_character or size_too_small):
 					line_height += 1
-					size_too_small = line_height - last_line_drawn < self.character_size
+					char_height = line_height - last_line_drawn
+					if last_was_too_big:
+						size_too_small = char_height < self.char_min_size - self.too_big_rectifier
+					else:
+						size_too_small = char_height < self.char_min_size
+
+					size_too_big = char_height > self.char_max_size
 					if not size_too_small:
 						intersects_character = check_interesects_character(line_height)
 
+				last_was_too_big = size_too_big
 				draw.line((left, line_height, right, line_height))
 				last_line_drawn = line_height
 
@@ -84,6 +95,7 @@ class NaiveSegmenter(Segmenter):
 	def get_characters(self):
 		characters = []
 		line_characters = []
+		temp = ""
 		im = self.get_screen_capture()
 		rgb_im = im.convert('RGB')
 		width, height = im.size
@@ -104,19 +116,30 @@ class NaiveSegmenter(Segmenter):
 
 			line_height = 0
 			last_line_drawn = 0
+			last_was_too_big = False
 			while line_height < height:
 				intersects_character = True
 				size_too_small = True
-				while intersects_character or size_too_small:
+				size_too_big = False
+				while not size_too_big and (intersects_character or size_too_small):
 					line_height += 1
-					size_too_small = line_height - last_line_drawn < self.character_size
+					char_height = line_height - last_line_drawn
+					if last_was_too_big:
+						size_too_small = char_height < self.char_min_size - self.too_big_rectifier
+					else:
+						size_too_small = char_height < self.char_min_size
+
+					size_too_big = char_height > self.char_max_size
 					if not size_too_small:
 						intersects_character = check_interesects_character(line_height)
 
+
 				character = im.crop((left, last_line_drawn, right, line_height))
 				line_characters.append(character)
-				#draw.line((left, line_height, right, line_height))
+				# if char_height > 30:
+				# 	temp += str(line_height - last_line_drawn) + "\n"
 				last_line_drawn = line_height
+				last_was_too_big = size_too_big
 
 			# We do top down but left to right
 			# So we reverse the vertical line so it's fully reversed:
@@ -130,7 +153,8 @@ class NaiveSegmenter(Segmenter):
 			char.save("images\\" + str(i) + ".jpg")
 			i += 1
 
-
+		# with open("temp.txt", "w") as f:
+		# 	f.write(temp)
 
 		# im = self.get_screen_capture()
 		# width, height = im.size
@@ -145,8 +169,9 @@ class NaiveSegmenter(Segmenter):
 if __name__ == "__main__":
 	bbox = (212, 155, 655, 950)
 	line_width = 45
-	char_size = 26
-	NS = NaiveSegmenter(bbox, line_width, char_size)
-	#NS.get_segmentation_visualization()
+	char_min_size = 26
+	char_max_size = 32
+	NS = NaiveSegmenter(bbox, line_width, char_min_size, char_max_size)
+	NS.get_segmentation_visualization()
 	NS.get_characters()
 
