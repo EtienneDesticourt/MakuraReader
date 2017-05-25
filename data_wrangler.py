1b9d0b7b1e04151846e6544ebb7ed90beb998f5e
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-05-25 15:18:08 +0200

    Created generator to replace Keras' builtin ones
    because flow from directory can't handle multiclass labels
    generator needed to load batch of images

--- data_wrangler.py
+++ data_wrangler.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import scipy
 from keras.preprocessing.image import ImageDataGenerator
 import config
 
@@ -39,3 +40,86 @@ class DataWrangler(object):
         return validation_generator
 
 
+class CustomDataWrangler(DataWrangler):
+
+    def get_train_generator(self):
+        with open("training.csv") as f: #clean
+            data = f.read().split("\n")
+        files = []
+        data.pop()
+        for line in data:
+            file_name, label = line.split("\t")
+            label_array = np.fromstring(label[1:-1], dtype=int, sep=",")
+            file_path = os.path.join(self.train_path, file_name)
+            files.append((file_path, label_array))
+
+        class Gen():
+            def __init__(gen):
+                gen.i = range(0, len(files), self.batch_size)
+                gen.j = 0
+
+            def __len__(gen):
+                return 2
+
+            def __iter__(gen):
+                return gen
+
+            def __next__(gen):
+                i = gen.i[gen.j]
+                images = np.zeros((self.batch_size, self.image_size[0], self.image_size[1], 1))
+                labels = np.zeros((self.batch_size, 236)) #clean
+                j = 0
+                for path, label in files[i:i+self.batch_size]:
+                    image = scipy.misc.imread(path, mode='L')
+                    image = image.reshape((self.image_size[0], self.image_size[1], 1))
+                    images[j] = image
+                    labels[j] = label
+                    j += 1
+                    
+                gen.j += 1
+                if gen.j >= len(gen.i):
+                    gen.j = 0
+                return images, labels
+
+        return Gen()
+
+    def get_val_generator(self):
+        with open("validation.csv") as f: #clean
+            data = f.read().split("\n")
+        data.pop()
+        files = []
+        for line in data:
+            file_name, label = line.split("\t")
+            label_array = np.fromstring(label[1:-1], dtype=int, sep=",")
+            file_path = os.path.join(self.val_path, file_name)
+            files.append((file_path, label_array))
+
+        class Gen():
+            def __init__(gen):
+                gen.i = range(0, len(files), self.batch_size)
+                gen.j = 0
+
+            def __len__(gen):
+                return 2
+
+            def __iter__(gen):
+                return gen
+
+            def __next__(gen):
+                i = gen.i[gen.j]
+                images = np.zeros((self.batch_size, self.image_size[0], self.image_size[1], 1))
+                labels = np.zeros((self.batch_size, 236)) #clean
+                j = 0
+                for path, label in files[i:i+self.batch_size]:
+                    image = scipy.misc.imread(path, mode='L')
+                    image = image.reshape((self.image_size[0], self.image_size[1], 1))
+                    images[j] = image
+                    labels[j] = label
+                    j += 1
+
+                gen.j += 1
+                if gen.j >= len(gen.i):
+                    gen.j = 0
+                return images, labels
+
+        return Gen()
--- trainer.py
+++ trainer.py
@@ -1,34 +1,50 @@
 from keras.callbacks import ModelCheckpoint
-from data_wrangler import DataWrangler
+from data_wrangler import DataWrangler, CustomDataWrangler
 from cnn import KanjiRecognizer
 from cnn import ElementRecognizer
+from cnn import ElementClassifier
+from cnn import InceptionV3Model
 import config
 
 
 if __name__ == "__main__":
     CLASSES = 236
-    NUM_TRAIN_SAMPLES = 732000#18881 #5160
+    NUM_TRAIN_SAMPLES = 30355#18881 #5160
+    NUM_VAL_SAMPLES = 7572
     #cnn = KanjiRecognizer(CLASSES, config.IMAGE_SIZE[0], config.LEARNING_RATE, config.EPOCHS)
-    #cnn.load_model("models\\kanji_only_2fc\\CNN_FULL.01-0.980.h5")
-    cnn = ElementRecognizer(CLASSES, config.IMAGE_SIZE[0], config.LEARNING_RATE, config.EPOCHS)
+    cnn = ElementClassifier(CLASSES, config.IMAGE_SIZE[0], config.LEARNING_RATE, config.EPOCHS)
+    #cnn = InceptionV3Model(CLASSES, config.IMAGE_SIZE[0], config.LEARNING_RATE, config.EPOCHS)
+    #cnn.load_model("CNN_ELEMENT_INC.10-0.257-2.280-0.660-0.915.h5")
     cnn.build_model()
 
-    dw = DataWrangler(train_path="C:\\DatasetCache\\data10\\train", val_path="C:\\DatasetCache\\data10\\val")
+    # cnn2 = InceptionV3Model(CLASSES, config.IMAGE_SIZE[0], config.LEARNING_RATE, config.EPOCHS)
+    # cnn2.load_model("CNN_ELEMENT_INC.09-0.996-0.039-0.996-0.041.h5")
+    # cnn.set_weights(cnn2.get_weights())
+    # del cnn2
+
+    dw = CustomDataWrangler(image_size=(64, 64), train_path="C:\\DatasetCache\\element_antialias_wb_modern\\train", val_path="C:\\DatasetCache\\element_antialias_wb_modern\\val")
+    #dw = DataWrangler(image_size=(64, 64), train_path="C:\\DatasetCache\\element_antialias_wb_modern\\train", val_path="C:\\DatasetCache\\element_antialias_wb_modern\\val")
 
     def get_save_callback():
-        model_name = "CNN_ELEMENT_FULL.{epoch:02d}-{val_acc:.3f}.h5"
+        model_name = "CNN_ELEMENTS_TRUNC_M7.{epoch:02d}-{val_acc:.3f}-{val_loss:.3f}-{acc:.3f}-{loss:.3f}.h5"
         return ModelCheckpoint(model_name, monitor='val_acc', verbose = 1, save_best_only = False)
 
     #class_weights = {0:34, 1: 1, 2: 34}
 
     train_gen = dw.get_train_generator()
     val_gen = dw.get_val_generator()
+    for i in train_gen:
+        print(type(i))
+        print(i[0].shape, i[1].shape)
+        break
+    input()
+    #print(type(train_gen))
 
     save_callback = get_save_callback()
 
     cnn.fit(
         train_gen,
         val_gen,
-        NUM_TRAIN_SAMPLES,
-        dw.num_val_samples,
+        NUM_TRAIN_SAMPLES/config.BATCH_SIZE,
+        NUM_VAL_SAMPLES/config.BATCH_SIZE,
         [save_callback])
