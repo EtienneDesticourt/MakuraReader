1f2b27183fd5a66dd6ea0e6c9d78373ce976852f
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-06-10 18:37:39 +0200

    Changed trainer for ectl

--- trainer.py
+++ trainer.py
@@ -1,88 +1,73 @@
+import os
+import sklearn.utils
+import sklearn.model_selection
 from keras.callbacks import ModelCheckpoint
-from data_wrangler import DataWrangler, CustomDataWrangler, E2KGenerator
-from cnn import KanjiRecognizer
-from cnn import ElementRecognizer
-from cnn import ElementClassifier
-from cnn import InceptionV3Model
-import config
-from e2k_classifier import E2KClassifier
+from keras.models import load_model
+
+from utils.ectl_loader import load_data
+from models.cnn_classifiers import M7_2, alphabet_classifier
+from keras.utils import np_utils
+import numpy as np
+
+from utils.misc import kanji_to_alphabet
 
 
-if __name__ == "__main__":
-    CLASSES = 128
-    NUM_TRAIN_SAMPLES = 120000#46197 ##7136#30355#18881 #5160
-    NUM_VAL_SAMPLES = 15000#11528 ##1659#7572
-    #cnn = KanjiRecognizer(CLASSES, config.IMAGE_SIZE[0], config.LEARNING_RATE, config.EPOCHS)
-    #cnn = ElementClassifier(CLASSES, config.IMAGE_SIZE[0], config.LEARNING_RATE, config.EPOCHS)
-    #cnn = InceptionV3Model(CLASSES, config.IMAGE_SIZE[0], config.LEARNING_RATE, config.EPOCHS)
-    #cnn.load_model(config.MODEL_TO_LOAD)
-    #cnn.build_model()
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" # Disable warnings
+DATASET_PATH = "data\\ETL9B\\ETL9B_1"
+TEST_SPLIT_PERCENTAGE = 0.2
+NUM_CLASSES = 3
+NUM_EPOCHS = 25
+BATCH_SIZE = 32
 
-    # cnn2 = InceptionV3Model(CLASSES, config.IMAGE_SIZE[0], config.LEARNING_RATE, config.EPOCHS)
-    # cnn2.load_model("CNN_ELEMENT_INC.09-0.996-0.039-0.996-0.041.h5")
-    # cnn.set_weights(cnn2.get_weights())
-    # del cnn2
-
-    #dw = CustomDataWrangler(image_size=(64, 64), train_path="C:\\DatasetCache\\all_kanjis_all_fonts_bw\\train", val_path="C:\\DatasetCache\\all_kanjis_all_fonts_bw\\val")
-
-    # TEMP REMOVE FROM HERE ON 
-
-    # Read kanji element decomposition dataset
-    with open("data\\kanji_elements.txt", "r", encoding="utf8") as f:
-        data = f.read().split("\n")
-    while "" in data:
-        data.remove("")
-    elements_ordered = []
-    kanjis = []
-    kanji_elements = {}
-    elements = {}
-    for kanji in data:
-        k, es = kanji.split(":")
-        kanjis.append(k)
-        kanji_elements[k] = es
-        for e in es:
-            if e not in elements:
-                elements[e] = []
-                elements_ordered.append(e)
-            elements[e].append(k)
-    remove_elements = []
-    for i in elements:
-        if len(elements[i]) < 15:
-            remove_elements.append(i)
-    for i in remove_elements:
-        elements.pop(i)
-        elements_ordered.remove(i)
-    # TEMP REMOVE ABOVE  NO FURTHER
+FILE_PATHS = ["data\\ETL9B\\ETL9B_1",
+              "data\\ETL9B\\ETL9B_2",
+              "data\\ETL9B\\ETL9B_3",
+              "data\\ETL9B\\ETL9B_4"] # Keep 5th for testing
 
 
+print("Compiling model.")
+#model = M7_2(n_output=NUM_CLASSES, input_shape=(64, 64, 1))
+model = load_model("CNN_K_M7_2_A.06-0.996-0.010.h5")
+#model = alphabet_classifier("CNN_K_M7_2.21-0.987-0.041.h5")
 
-    classes = elements_ordered
-    dw = DataWrangler(image_size=(64, 64), 
-        train_path="C:\\DatasetCache\\all_kanjis_all_fonts_bw\\train", 
-        val_path="C:\\DatasetCache\\all_kanjis_all_fonts_bw\\val")
+
+for path in FILE_PATHS[3:]:
+    print("Loading ECTL data.", path)
+    x, y = load_data(path, truncate=10000, alphabet=True, verbose=True)
+    
+
+    print("Shuffling data.")
+    x_shuffled, y_shuffled = sklearn.utils.shuffle(x, y, random_state=0)
+    del x
+    del y
+
+    # print("Splitting train and test data.")
+    # x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x_shuffled, y_shuffled, test_size=TEST_SPLIT_PERCENTAGE, random_state=42)
+    # del x_shuffled
+    # del y_shuffled
 
     def get_save_callback():
-        model_name = "CNN_E2K_TRUNC_M7.{epoch:02d}-{val_acc:.3f}-{val_loss:.3f}-{acc:.3f}-{loss:.3f}.h5"
-        return ModelCheckpoint(model_name, monitor='val_acc', verbose = 1, save_best_only = False)
-
-    #class_weights = {0:34, 1: 1, 2: 34}
-
-    train_gen = dw.get_train_generator()
-    val_gen = dw.get_val_generator()
-
-    #e2k_train_gen = E2KGenerator(cnn, train_gen)
-    #e2k_val_gen = E2KGenerator(cnn, val_gen)
+            model_name = "CNN_K_M7_2_A.{epoch:02d}-{acc:.3f}-{loss:.3f}.h5"
+            return ModelCheckpoint(model_name, monitor='acc', verbose=1, save_best_only=False)
 
 
-    e2k = E2KClassifier(config.MODEL_TO_LOAD, config.LEARNING_RATE, config.EPOCHS)
-    e2k.load_model("CNN_E2K_TRUNC_M7.03-0.000-7.859-0.000-7.827.h5")
+    print("Training model.")
+    model.fit(x_shuffled, y_shuffled,
+              epochs=NUM_EPOCHS,
+              batch_size=BATCH_SIZE,
+              callbacks= [get_save_callback()])
 
+    del x_shuffled
+    del y_shuffled
 
-    save_callback = get_save_callback()
+# save_model_weights('weights/M7_2-kanji_weights.h5', model)
 
-    e2k.fit(
-        train_gen,
-        val_gen,
-        NUM_TRAIN_SAMPLES,
-        NUM_VAL_SAMPLES,
-        [save_callback])
+# print("Evaluating model.")
+# score, acc = model.evaluate(x_test, y_test,
+#                             batch_size=128,
+#                             verbose=0)
+
+# print( "Training size: ", x_train.shape[0])
+# print( "Test size: ", x_test.shape[0])
+# print( "Test Score: ", score)
+# print( "Test Accuracy: ", acc)
