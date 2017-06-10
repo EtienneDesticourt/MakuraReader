17071ff2862516bd46f0effa283ddd6a86eed556
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-06-11 01:41:18 +0200

    Fixed trainer for alphabet discrimination

--- utils/misc.py
+++ utils/misc.py
@@ -1,28 +1,28 @@
 import numpy as np
 from keras.utils import np_utils
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 
 def jis_code_to_alphabet(labels):
-	new_labels = np.zeros((labels.shape[0], 3))
-	print(new_labels.shape)
-	# Hiragana
-	hiragana = np.logical_and(labels >= 0x2421, labels <= 0x2473)
-	print("Hiragana:",sum(hiragana))
-	new_labels[hiragana] = np.array([1, 0, 0])
-	# Katakana
-	katakana = np.logical_and(labels >= 0x2521, labels <= 0x2576)
-	print("Katakana:",sum(katakana))
-	new_labels[katakana] = [0, 1, 0]
-	# Kanji
-	kanji = np.logical_and(np.logical_not(hiragana), np.logical_not(katakana))
-	print("Kanji:", sum(kanji))
-	new_labels[kanji] = [0, 0, 1]
+    new_labels = np.zeros((labels.shape[0], 3))
+    print(new_labels.shape)
+    # Hiragana
+    hiragana = np.logical_and(labels >= 0x2421, labels <= 0x2473)
+    print("Hiragana:",sum(hiragana))
+    new_labels[hiragana] = np.array([1, 0, 0])
+    # Katakana
+    katakana = np.logical_and(labels >= 0x2521, labels <= 0x2576)
+    print("Katakana:",sum(katakana))
+    new_labels[katakana] = [0, 1, 0]
+    # Kanji
+    kanji = np.logical_and(np.logical_not(hiragana), np.logical_not(katakana))
+    print("Kanji:", sum(kanji))
+    new_labels[kanji] = [0, 0, 1]
 
-	return new_labels
+    return new_labels
 
 
-def get_simple_image_processer(image_size, inverted=False):	
+def get_simple_image_processer(image_size, inverted=False): 
     background = Image.new('1', image_size)
     def process_image(image):
         background.paste(image) # 64x63 to 64x64
@@ -34,35 +34,51 @@ def get_simple_image_processer(image_size, inverted=False):
     return process_image
 
 
-def jis_code_to_categorical(labels):	
+def get_kata_image_processor(image_size, inverted=True):
+    background = Image.new('1', image_size)
+    def process_image(image):
+        image = image.convert('P')
+        enhancer = ImageEnhance.Brightness(image)
+        image = enhancer.enhance(40)
+        background.paste(image) # 64x63 to 64x64
+        if inverted:
+            new_image = Image.eval(background, lambda x: not x)
+        else:
+            new_image = background
+        return background
+    return process_image
+
+
+
+def jis_code_to_categorical(labels):    
     unique_labels = list(set(labels))
     labels_dict = {unique_labels[i]: i for i in range(len(unique_labels))}
     new_labels = np.array([labels_dict[l] for l in labels], dtype=np.int32)
     y = np_utils.to_categorical(new_labels, len(unique_labels))
-    return y, unique_labels
+    return y, np.array(unique_labels)
 
 def JIS_201_to_208(jis_201_code):
     table = {166: 0x2572,
-    		 168: 0x2523,
-    		 170: 0x2527,
-    		 177: 0x2522,
-    		 178: 0x2524,
-    		 179: 0x2526,
-    		 180: 0x2528,
-    		 181: 0x2529,
-    		 182: 0x252b,
-    		 183: 0x252d,
-    		 184: 0x252f,
-    		 185: 0x2531,
-    		 186: 0x2533,
-    		 187: 0x2535,
-    		 188: 0x2537,
-    		 189: 0x2539,
-    		 190: 0x253b,
-    		 191: 0x253d,
-    		 192: 0x253f,
-    		 193: 0x2541,
-    		 194: 0x2544,
+             168: 0x2523,
+             170: 0x2527,
+             177: 0x2522,
+             178: 0x2524,
+             179: 0x2526,
+             180: 0x2528,
+             181: 0x2529,
+             182: 0x252b,
+             183: 0x252d,
+             184: 0x252f,
+             185: 0x2531,
+             186: 0x2533,
+             187: 0x2535,
+             188: 0x2537,
+             189: 0x2539,
+             190: 0x253b,
+             191: 0x253d,
+             192: 0x253f,
+             193: 0x2541,
+             194: 0x2544,
              195: 0x2546,
              196: 0x2548,
              197: 0x254a,
