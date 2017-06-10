61c8bc20bdfc75a67794a986a16420177df13b50
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-06-10 22:33:26 +0200

    Added JIS 201 to 208 converter

--- utils/misc.py
+++ utils/misc.py
@@ -1,26 +1,22 @@
-
-# def reverse_one_hot(labels, unique_classes):
-# 	num_classes = len(unique_classes)
-# 	argmax(labels, axis=1)
-
 import numpy as np
 from keras.utils import np_utils
 from PIL import Image
+
 
 def jis_code_to_alphabet(labels):
 	new_labels = np.zeros((labels.shape[0], 3))
 	print(new_labels.shape)
 	# Hiragana
 	hiragana = np.logical_and(labels >= 0x2421, labels <= 0x2473)
-	print(sum(hiragana))
+	print("Hiragana:",sum(hiragana))
 	new_labels[hiragana] = np.array([1, 0, 0])
 	# Katakana
 	katakana = np.logical_and(labels >= 0x2521, labels <= 0x2576)
-	print(sum(katakana))
+	print("Katakana:",sum(katakana))
 	new_labels[katakana] = [0, 1, 0]
 	# Kanji
 	kanji = np.logical_and(np.logical_not(hiragana), np.logical_not(katakana))
-	print(sum(kanji))
+	print("Kanji:", sum(kanji))
 	new_labels[kanji] = [0, 0, 1]
 
 	return new_labels
@@ -44,3 +40,54 @@ def jis_code_to_categorical(labels):
     new_labels = np.array([labels_dict[l] for l in labels], dtype=np.int32)
     y = np_utils.to_categorical(new_labels, len(unique_labels))
     return y, unique_labels
+
+def JIS_201_to_208(jis_201_code):
+    table = {166: 0x2572,
+    		 168: 0x2523,
+    		 170: 0x2527,
+    		 177: 0x2522,
+    		 178: 0x2524,
+    		 179: 0x2526,
+    		 180: 0x2528,
+    		 181: 0x2529,
+    		 182: 0x252b,
+    		 183: 0x252d,
+    		 184: 0x252f,
+    		 185: 0x2531,
+    		 186: 0x2533,
+    		 187: 0x2535,
+    		 188: 0x2537,
+    		 189: 0x2539,
+    		 190: 0x253b,
+    		 191: 0x253d,
+    		 192: 0x253f,
+    		 193: 0x2541,
+    		 194: 0x2544,
+             195: 0x2546,
+             196: 0x2548,
+             197: 0x254a,
+             198: 0x254b,
+             199: 0x254c,
+             200: 0x254d,
+             201: 0x254e,
+             202: 0x254f,
+             203: 0x2552,
+             204: 0x2555,
+             205: 0x2558,
+             206: 0x255b,
+             207: 0x255e,
+             208: 0x255f,
+             209: 0x2560,
+             210: 0x2561,
+             211: 0x2562,
+             212: 0x2564,
+             213: 0x2566,
+             214: 0x2568,
+             215: 0x2569,
+             216: 0x256A,
+             217: 0x256B,
+             218: 0x256C,
+             219: 0x256D,
+             220: 0x256F,
+             221: 0x2573,}
+    return table[jis_201_code]
