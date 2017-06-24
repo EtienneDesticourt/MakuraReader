3664700905eec52444becde4f040b193d45ba156
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-06-24 12:39:07 +0200

    Minor changes

--- models/trainer.py
+++ models/trainer.py
@@ -31,19 +31,22 @@ ETL1_PATHS = ["data\\ETL1C\\ETL1C_07",
               "data\\ETL1C\\ETL1C_12",
               "data\\ETL1C\\ETL1C_13",]
 
-CURRENT_RUN = 3 #Still gotta do #3 out of 4
+CURRENT_RUN = 4 #Done except 5
 CURRENT_ETL9_PATH = ETL9_PATHS[CURRENT_RUN]
+
+NUM_RECORDS_PER_CHAR = 200
 
 GENERATE = True
 
 if GENERATE:
-    process_9B_image = get_simple_image_processer(IMAGE_SIZE, inverted=True)
-    process_1C_image = get_kata_image_processor(IMAGE_SIZE, inverted=True)
+    process_9B_image = get_simple_image_processer(IMAGE_SIZE, inverted=False)
+    process_1C_image = get_kata_image_processor(IMAGE_SIZE, inverted=False)
 
     # Load hiragana and kanji
     # 40 records per character per file
     # Only enough memory for one file at a time
     x, y = load_data(CURRENT_ETL9_PATH, Record9B, process_image=process_9B_image)
+    # x, y = np.empty((0, 64, 64, 1)), np.empty((0,)) # For testing kata only
 
     # Load katakana
     # 1411 records per character per file
@@ -51,24 +54,27 @@ if GENERATE:
     # all records for each characters are bundled together
     # meaning records 0 to 1410 are A, 1411 to 2821 are B, etc ...
     # We want 40 records per char so we load a bit of each file
-    record_range_40 = []
+    record_range = []
     # We load the 40 first records out of 1411 for each character.
     # On subsequent runs, as we load new kanji and hiragana from the other ETL9 files
     # We increase the current run offset to load a new set of 40 records for each char
-    for i in range(8): record_range_40 += list(range(1411*i + 40*CURRENT_RUN, 1411*i + 40*(CURRENT_RUN+1)))
+    for i in range(8): record_range += list(range(1411*i + NUM_RECORDS_PER_CHAR*CURRENT_RUN, 1411*i + NUM_RECORDS_PER_CHAR*(CURRENT_RUN+1)))
     for path in ETL1_PATHS:
-        if path == ETL1_PATHS[-1]: record_range = record_range_40[:40*3] # Only 3 characters in last file
-        else: record_range = record_range_40 # 8 characters in other files
+        if path == ETL1_PATHS[-1]: curr_record_range = record_range[:NUM_RECORDS_PER_CHAR*3] # Only 3 characters in last file
+        else: curr_record_range = record_range # 8 characters in other files
 
-        nx, ny = load_data(path, Record1C, record_range=record_range, process_image=process_1C_image)
+        # print(path)
+
+        nx, ny = load_data(path, Record1C, record_range=curr_record_range, process_image=process_1C_image)
         ny = np.apply_along_axis(lambda x: JIS_201_to_208(x[0]), 0, [ny])
 
         x = np.concatenate((x, nx))
         y = np.concatenate((y, ny))
 
 
-    y, uniques = jis_code_to_categorical(y)
-    np.save("uniques3084.npy", uniques)
+    uniques = np.load("uniques3084_bu.npy")
+    y, uniques = jis_code_to_categorical(y, uniques)
+    # np.save("uniques3084.npy", uniques)
 
     print("Shuffling data.")
     x_shuffled, y_shuffled = sklearn.utils.shuffle(x, y, random_state=0)
@@ -89,7 +95,7 @@ print("Compiling model.")
 # Not sure why, maybe katakana redundant with kanji or hiragana
 # weird stuff, don't really care to take the time to find out
 #model = M7_2(n_output=3084, input_shape=(64, 64, 1))
-model = load_model("CNN_FULL_M7_2.09-0.963-0.117.h5")
+model = load_model("CNN_FULL_M7_2.09-0.979-0.069.h5")
 #model = alphabet_classifier("CNN_K_M7_2.21-0.987-0.041.h5")
 
 print("Splitting train and test data.")
